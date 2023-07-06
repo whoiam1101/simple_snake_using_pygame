7842c41314fc1f5e165afe8df19780a73d44ed86
whoiam1101 <whoiam1101@outlook.com>
2023-07-06 13:40:45 +0400

    added my custom style(8-bit text, no title, setting background color)

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_menu
 
+from colors import SCREEN_BACKGROUN_COLOR
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from game import game
 
@@ -13,10 +14,19 @@ pygame.init()
 screen: pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
-menu: pygame_menu.Menu = pygame_menu.Menu('Hi Ilya',
+menu_theme: pygame_menu.Theme = pygame_menu.Theme(background_color=SCREEN_BACKGROUN_COLOR,
+                                                  border_color=SCREEN_BACKGROUN_COLOR,
+                                                  cursor_color="green",
+                                                  selection_color="green",
+                                                  title=False,
+                                                  # title_shadow=True,
+                                                  widget_font=pygame_menu.font.FONT_8BIT)
+
+
+menu: pygame_menu.Menu = pygame_menu.Menu('Snake',
                                           width=SCREEN_WIDTH,
                                           height=SCREEN_HEIGHT,
-                                          theme=pygame_menu.themes.THEME_DARK)
+                                          theme=menu_theme)
 
 menu.add.button('Play', game)
 menu.add.button('Quit', quit)
